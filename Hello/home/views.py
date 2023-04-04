0798a0783ff9de32534a61d00ae1ff78bfc3988a
Utkarsh Pandey <35457627+UtkarshPandeyGCI@users.noreply.github.com>
2023-04-04 20:07:21 +0530

    Add files via upload

---
+++ Hello/home/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("This is homepage")
+    context = {
+        "variable1":"Utkarsh is Great",
+        "variable2":"Django coding"
+    }
+    messages.success(request, "This is a test message")
+    return render(request,'index.html',context)
+
+def about(request):
+    #return HttpResponse("This is About Page")
+    return render(request,'about.html')
+
+def services(request):
+    #return HttpResponse("This is Services Page")
+    return render(request,'services.html')
+
+def contact(request):
+    #return HttpResponse("This is contact Page")
+    if request.method=="POST": #runs only if POST request will be rcvd
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request,'contact.html')
